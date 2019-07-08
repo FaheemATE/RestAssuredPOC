f793135f3d7e6e395ff4bd635dbf0d65455d4857
Mohammad Faheem <Fahimmd333@gmail.com>
2019-07-08 21:34:13 +0530

    Add files via upload

---
+++ RestAssuredPOC/src/test/java/BasicDemo/BasicClass.java
@@ -0,0 +1,18 @@
+package BasicDemo;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+
+public class BasicClass {
+	
+	@Test
+	public void basicTest()
+	{
+		
+		
+
+		   
+	}
+
+}
